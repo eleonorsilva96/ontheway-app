6e5682cc49f85a3348504a139f8d78084f6c148d
EleonorSilva <eleonorsilva123@gmail.com>
2019-01-31 15:20:49 +0000

    detalhe condutor

--- src/js/components/Condutor.js
+++ src/js/components/Condutor.js
@@ -19,12 +19,12 @@ class Condutor extends React.Component{
         this.myElement = null;
         this.myTween = null;
     }
-    /*
+
     componentDidMount(){
         // use the node ref to create the animation
-        this.myTween = TweenLite.to(this.myElement, 1, {display:"none"});
+        this.myTween = TweenLite.to(this.myElement, 1, {ease:Power3.easeOut, autoAlpha:0}, '-=200');
     }
-*/
+
     render (){
         return(
             <div className="container-fluid h-100 p-0">
@@ -34,61 +34,89 @@ class Condutor extends React.Component{
 
                         <div className="row d-flex justify-content-center align-items-center">
                             <div className="col-4 d-flex flex-column align-items-center">
-                                <h5 className="d-flex text-uppercase align-items-center font-weight-bold gray-text">Produto<span id="num-produto">03</span></h5>
-                                <h5 id="nome-produto" className="d-flex text-uppercase align-items-center font-weight-bold primary-text">Poltrona</h5>
+                                <h5 className="d-flex text-uppercase align-items-center font-weight-bold gray-text">Produto<span className="h6 pl-1" id="num-produto">03</span></h5>
+                                <h3 id="nome-produto" className="h3 d-flex text-uppercase align-items-center font-weight-bold primary-text">Poltrona</h3>
                                 <h6 className="d-flex text-uppercase align-items-center font-weight-bold gray-text">(<span id="tamanho-produto">grande</span>)</h6>
                             </div>
                             <div className="col-4 d-flex flex-column align-items-center align-self-start">
                                 <h5 className="d-flex text-uppercase align-items-center font-weight-bold gray-text">Preço</h5>
-                                <h5 id="preco-produto" className="d-flex text-uppercase align-items-center font-weight-bold primary-text">Poltrona</h5>
+                                <h3 className=" h3 d-flex text-uppercase align-items-center font-weight-bold primary-text"><span id="preco-produto">15</span>€</h3>
                             </div>
                         </div>
 
-                        <div className="d-flex row justify-content-center align-items-center mt-5">
+                        <div className="d-flex row justify-content-center align-items-center mt-4 mb-4">
                             <div className="col-6 d-flex flex-column justify-content-start align-items-start">
-                                <div className="d-flex row align-items-center"><label>De</label>
-                                    <h6 id="origem" className="ml-3">IKEA, Porto</h6></div>
-                                <div className="d-flex row align-items-center"><label>Para</label>
-                                    <h6 className="ml-3" id="destino">Campus Santiago, Aveiro</h6></div>
+                                <div className="d-flex row align-items-center pb-3">
+                                    <label className="font-weight-bold gray-text">De</label>
+                                    <div id="origem" className="h6 ml-3 primary-3-text">IKEA, Porto</div>
+                                </div>
+                                <div className="d-flex row align-items-center">
+                                    <label className="font-weight-bold gray-text">Para</label>
+                                    <div className="h6 ml-3 primary-3-text" id="destino">Campus Santiago, Aveiro</div></div>
                             </div>
-                            <div className="col-3 d-flex flex-column justify-content-between align-items-between">
-                                <div className="d-flex row pb-2"><label className="icon-time"></label>
-                                    <h6 id="tempo"><span id="inicio">19:00</span>:<span id="fim">20:00</span></h6></div>
-                                <div className="d-flex row"><label className="icon-calender"></label><h6 id="data">19/12/18</h6></div>
+                            <div className="col-4 pl-5 pr-0 d-flex flex-column justify-content-between align-items-between">
+                                <div className="d-flex row pb-2 align-items-center pb-3">
+                                    <label className="icon-time mr-1">
+                                    </label>
+                                    <div className="h6 primary-3-text" id="tempo"><span id="inicio">19:00</span>-<span id="fim">20:00</span></div>
+                                </div>
+                                <div className="d-flex row align-items-center">
+                                    <label className="icon-calender mr-1">
+                                    </label>
+                                    <div className="h6 primary-3-text" id="data">19/12/18</div></div>
                             </div>
                         </div>
 
-                        <div className="white">
-                                <div className="row d-flex align-items-center">
-                                    <div className="d-flex flex-column align-items-center">
-                                        <div className="m-3 previewComponent-md">
-                                            <div id="image-default" className="image-default">
-                                            </div>
-                                        </div>
-                                        <h6 className="text-uppercase font-weight-bold">tempo médio</h6>
-                                        <h6 className="text-uppercase font-weight-bold"><span id="tempo-medio">35</span>min</h6>
-                                    </div>
-                                    <div className="d-flex flex-column align-items-center">
-                                        <div className="row d-flex align-items-center">
-                                            <div className="d-flex flex-column align-items-center">
-                                                <h5 className="text-uppercase font-weight-bold">viagens realizadas</h5>
-                                                <div className="w-25 rounded-circle white shadow"><span className="primary-text" id="nr-viagens">6</span></div>
-                                            </div>
-                                            <div className="d-flex flex-column align-items-center">
-                                                <h5 className="text-uppercase font-weight-bold">amigos em comum</h5>
-                                                <div className="w-25 rounded-circle white shadow"><span className="primary-text" id="nr-amigos">6</span></div>
-                                            </div>
+                        <div className="white border-primary-white rounded ml-3 mr-3">
+                            <div className="row d-flex justify-content-center align-items-center">
+                                <div className="pt-5 mr-4 col-2 align-self-start d-flex flex-column align-items-center">
+                                    <div className="mb-3 previewComponent-gd">
+                                        <div id="image-default" className="image-default">
                                         </div>
                                     </div>
+                                    <h6 className="h6 pb-1 align-self-start text-uppercase font-weight-bold">tempo<br/>médio</h6>
+                                    <span className="font-xs align-self-start text-uppercase font-weight-bold primary-text"><span id="tempo-medio font-weight-bold">35</span>min</span>
                                 </div>
-                                <div className="row d-flex align-items-center">
+                                <div className="pt-4 col-7 d-flex flex-column align-items-center">
+                                    <div className="row d-flex align-items-center">
+                                        <div className="m-3 d-flex flex-column align-items-center">
+                                            <h5 className="h4 pb-3 text-uppercase font-weight-bold text-center">viagens<br/>realizadas</h5>
+                                            <div className="circle-size d-flex align-items-center justify-content-center rounded-circle white shadow">
+                                                <span id="nr-viagens" className="h5 primary-text font-weight-bold">6</span>
+                                            </div>
+                                        </div>
+                                        <div className="d-flex m-3 flex-column align-items-center">
+                                            <h5 className="h4 pb-3 text-uppercase font-weight-bold text-center">amigos<br/>em comum</h5>
+                                            <div className="circle-size d-flex align-items-center justify-content-center rounded-circle white shadow">
+                                                <span id="nr-amigos" className="h5 primary-text font-weight-bold">6</span>
+                                            </div>
+                                        </div>
+                                        <div className="pl-5 pt-2 d-flex flex-column align-items-center">
+                                            <div className="row align-items-center align-self-start">
+                                                <h5 className="text-uppercase align-items-center font-weight-bold primary-text">Zé Pedro</h5>
+                                                <div className="icon-star">
+                                                </div>
+                                                <span id="review" className="font-weight-bold">3.5</span>
+                                            </div>
+                                            <div className="d-flex row align-items-center">
+                                                <p className="text-left">Vamos poupar tempo e dinheiro? E o nosso ambiente também!</p>
+                                            </div>
+                                        </div>
 
+                                        <div className="mt-4 mb-4 row justify-content-center align-self-center">
+                                            <div className="m-2 row align-items-center primary-btn primary white-text pedido justify-content-center blue-btn">
+                                                <Link className="d-flex justify-content-center align-items-center link-no-decoration white-text text-uppercase font-weight-bold" to="/condutores/">
+                                                    Aceitar
+                                                </Link> </div>
+                                        </div>
+                                    </div>
                                 </div>
+                            </div>
                         </div>
 
                     </div>
                 </div>
-                <FooterPath />
+                <FooterPath pathRef={div => this.myElement = div} />
             </div>
         );
     }
